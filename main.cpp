1eeb71740700588bb3418d131668ca02f3925f86
hsiaochuan <hsiaochuan@126.com>
2026-04-14 01:49:36 +0900

    save some file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include <fstream>
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Jenkins" << std::endl;
+    std::ofstream ofs;
+    ofs.open("./hello.txt");
+    ofs << "it is test data";
+    ofs.close();
     return 0;
 }
