3937bbadcdc7f6cd3b66bb6a415ba9f6bdc4e086
hsiaochuan <hsiaochuan@126.com>
2026-04-14 04:24:10 +0900

    new 2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Jenkins" << std::endl;
     std::cout << "new commit" << std::endl;
+    std::cout << "new commit 2" << std::endl;
     std::ofstream ofs;
     ofs.open("./hello.txt");
     ofs << "it is test data";
